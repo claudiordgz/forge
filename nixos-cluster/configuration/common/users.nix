c3a0d7dba8283d447d5a61bccd64f874726f752a
claudiordgz <claudio.rdgz@gmail.com>
2025-06-02 22:17:33 -0700

    extra flags

--- nixos-cluster/configuration/common/users.nix
+++ nixos-cluster/configuration/common/users.nix
@@ -5,9 +5,12 @@ let
 in
 {
   sops.secrets."sshKeys/${host}/adminuser" = {
+    neededForUsers = true
     path = "/etc/ssh/keys/vega-adminuser";
     mode = "0444";
   };
+
+  users.mutableUsers = false;
 
   users.users.root.openssh.authorizedKeys.keyFiles = [
     config.sops.secrets."sshKeys/${host}/adminuser".path
