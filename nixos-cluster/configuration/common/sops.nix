422e3ec802d02ed7315988157ff75099f8e06ce1
claudiordgz <claudio.rdgz@gmail.com>
2025-06-02 21:59:07 -0700

    fix path

--- nixos-cluster/configuration/common/sops.nix
+++ nixos-cluster/configuration/common/sops.nix
@@ -1,7 +1,7 @@
 {
   sops = {
     defaultSopsFormat = "yaml";
-    defaultSopsFile = ./configuration/secrets-encrypted.yaml;
+    defaultSopsFile = ../secrets-encrypted.yaml;
     validateSopsFiles = false; 
     age.keyFile = "/root/.config/sops/age/keys.txt"; 
   };
