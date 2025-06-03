033da9c780fa2795cc0089a1408d99ac00b1fb2d
claudiordgz <claudio.rdgz@gmail.com>
2025-06-02 23:08:17 -0700

    try raw

--- nixos-cluster/configuration/common/users.nix
+++ nixos-cluster/configuration/common/users.nix
@@ -18,6 +18,7 @@ in
     isNormalUser = true;
     extraGroups = [ "wheel" "networkmanager" "podman" ];
     openssh.authorizedKeys.keys = [
+      config.sops.secrets.${secretPath}.raw
     ];
   };
 }
