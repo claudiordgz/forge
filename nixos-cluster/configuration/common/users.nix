c1f300485ee99fcf49933d9b4ad42c4af10198f7
claudiordgz <claudio.rdgz@gmail.com>
2025-06-02 22:15:28 -0700

    try using files

--- nixos-cluster/configuration/common/users.nix
+++ nixos-cluster/configuration/common/users.nix
@@ -4,9 +4,12 @@ let
   host = config.networking.hostName;
 in
 {
-  sops.secrets."sshKeys/${host}/adminuser" = {};
+  sops.secrets."sshKeys/${host}/adminuser" = {
+    path = "/etc/ssh/keys/vega-adminuser";
+    mode = "0444";
+  };
 
-  users.users.root.openssh.authorizedKeys.keys = [
+  users.users.root.openssh.authorizedKeys.keyFiles = [
     config.sops.secrets."sshKeys/${host}/adminuser".path
   ];
 }
