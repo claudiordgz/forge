f633d1d71fbafff60294493c299cdb3f43288683
claudiordgz <claudio.rdgz@gmail.com>
2025-08-05 01:33:21 -0700

    ambiguous?

--- nixos-cluster/configuration/common/users.nix
+++ nixos-cluster/configuration/common/users.nix
@@ -1,26 +1,29 @@
-{ config, pkgs, lib, inputs, keys, ... }:
+{ config, pkgs, lib, inputs, ... }:
 
 let
   host = config.networking.hostName;
-  pub  = "${keys}/${host}-adminuser.pub";
+  pub  = "${inputs.keys}/${host}-adminuser.pub"; 
 in
 {
   services.openssh.enable = true;
 
-  # Make absolutely sure no one else sets keyFiles
+  # Belt-and-suspenders: ensure nobody else injects keyFiles
   users.users.admin.openssh.authorizedKeys.keyFiles = lib.mkForce [ ];
   users.users.root.openssh.authorizedKeys.keyFiles  = lib.mkForce [ ];
 
   users.users = {
     admin = {
-      isNormalUser = true;
-      extraGroups  = [ "wheel" "networkmanager" "podman" ];
+      isNormalUser  = true;
+      extraGroups   = [ "wheel" "networkmanager" "podman" ];
       openssh.authorizedKeys.keys = [ (builtins.readFile pub) ];
     };
+
     root.openssh.authorizedKeys.keys = [ (builtins.readFile pub) ];
   };
 
-  # Also ensure no stray /etc entries get created elsewhere:
-  environment.etc."ssh/authorized_keys.d/admin".enable = lib.mkForce false;
-  environment.etc."ssh/authorized_keys.d/root".enable  = lib.mkForce false;
+  # Optional: catch missing pub early with a clear message
+  assertions = [{
+    assertion = builtins.pathExists pub;
+    message   = "Missing pubkey at: ${pub}";
+  }];
 }
