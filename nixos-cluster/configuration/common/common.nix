8341a0ae53ecb5f027ddce6107a560ff10310c83
claudiordgz <claudio.rdgz@gmail.com>
2025-08-05 02:27:53 -0700

    also no

--- nixos-cluster/configuration/common/common.nix
+++ nixos-cluster/configuration/common/common.nix
@@ -45,10 +45,7 @@ in {
     nvidiaSettings = true;
   };
 
-  services.nvidia-persistenced.enable = true;
-
   hardware.nvidia-container-toolkit.enable = true;
-
 
   time.timeZone = "America/Los_Angeles";
   i18n.defaultLocale = "en_US.UTF-8";
