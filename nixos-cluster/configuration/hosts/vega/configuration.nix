01ebc81a6a6d76257da06968678cfca0a02c2ab3
claudiordgz <claudio.rdgz@gmail.com>
2025-08-08 14:48:54 -0700

    certs

--- nixos-cluster/configuration/hosts/vega/configuration.nix
+++ nixos-cluster/configuration/hosts/vega/configuration.nix
@@ -11,25 +11,27 @@
     valuesYAML = ''
       grafana:
         ingress:
-          annotations:
-            cert-manager.io/cluster-issuer: letsencrypt-prod
-        ingress:
           enabled: true
           ingressClassName: nginx
-          hosts: [ "grafana.locallier.com" ]
+          annotations:
+            cert-manager.io/cluster-issuer: letsencrypt-prod
+          hosts:
+            - "grafana.locallier.com"
           tls:
-            - hosts: [ "grafana.locallier.com" ]
+            - hosts:
+                - "grafana.locallier.com"
               secretName: grafana-tls
       prometheus:
         ingress:
-          annotations:
-            cert-manager.io/cluster-issuer: letsencrypt-prod
-        ingress:
           enabled: true
           ingressClassName: nginx
-          hosts: [ "prometheus.locallier.com" ]
+          annotations:
+            cert-manager.io/cluster-issuer: letsencrypt-prod
+          hosts:
+            - "prometheus.locallier.com"
           tls:
-            - hosts: [ "prometheus.locallier.com" ]
+            - hosts:
+                - "prometheus.locallier.com"
               secretName: prometheus-tls
     '';
   };
